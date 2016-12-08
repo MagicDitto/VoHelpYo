b6797debc5cda552bc3810065b6edb59771604a8
xt449 <alpha.xt449.omega@gmail.com>
2016-12-08 12:57:21 -0500

    code organization + clean up
    Signed-off-by: xt449 <alpha.xt449.omega@gmail.com>

--- main.js
+++ main.js
@@ -17,74 +17,65 @@ BOT.on('ready', () => {
 
 // Create an event listener for messages.
 BOT.on('message', message => {
-	// if the message contains the BOT's mention...
-	if (message.content.includes(BOT.user.toString())) {
+	// if the message starts with '!'...
+	if(message.content.startsWith('!')) {
+		const command = message.content.split(' ')[0];
+		
+		// switch on the first word of the message...
+		switch(command) {
+			'!avatar': {
+				//const timestamp = new Date().getTime() / 1000;
+				const avatarURL = message.author.avatarURL;
+				//const avatarID = message.author.avatar;
+				
+				message.channel.sendFile(avatarURL.toString(), '', message.author.toString()).catch(console.error);
+				break;
+			}
+		}
+	} else // if the message contains the BOT's mention...
+	if(message.content.includes(BOT.user.toString())) {
 		// .reply adds the user's mention to the begining of the message.
 		message.reply("Hello!");
-	} else {
-		// if the message is 'what is my avatar'...
-		//if (message.content === 'what is my avatar') {
+	}
 
-		// if the message contains 'what is my avatar'...
-		//if (message.content.includes('what is my avatar')) {
+	//message.channel.sendFile(avatarURL.toString()).catch(console.error);
 
-		// if the message starts with '!avatar'...
-		if (message.content.startsWith('!avatar')) {
-			var timestamp = new Date().getTime() / 1000;
-			var avatarURL = message.author.avatarURL;
-			var avatarID = message.author.avatar;
-			//var filename = avatarID + '.jpg';
-			//var filename = avatarURL.substring(avatarURL.lastIndexOf('/') + 1, avatarURL.length);
+	/*
+	message.reply(message.author.avatarURL);
+	console.log('SENT AVATAR URL');
+	WEBHOOK.sendMessage('hello!')
+	.catch(console.error);
+	*/
 
-			//console.log(avatarURL);
-			//console.log(avatarID);
-			//console.log(filename);
+	/*
+	WEBHOOK.sendSlackMessage({
+		'username': 'Wumpus',
+		'attachments': [{
+			'pretext': 'this looks pretty cool',
+			'color': '#F0F',
+			'footer_icon': 'http://snek.s3.amazonaws.com/topSnek.png',
+			'footer': 'Powered by sneks',
+			'ts': timestamp
+		}]
+	}).catch(console.error);*/
 
-			//console.log(message.author.toString());
-
-			// Send the user's avatar (URL).
-
-			//message.channel.sendFile(avatarURL.toString()).catch(console.error);
-
-			message.channel.sendFile(avatarURL.toString(), '', message.author.toString()).catch(console.error);
-
-			/*
-			message.reply(message.author.avatarURL);
-			console.log('SENT AVATAR URL');
-			WEBHOOK.sendMessage('hello!')
-			.catch(console.error);
-			*/
-
-			//WEBHOOK.sendFile(avatarURL.toString(), filename, message.author.username).catch(console.error);
-
-			//WEBHOOK.sendFile(avatarURL.toString()).catch(console.error);
-
-			/*
-			WEBHOOK.sendSlackMessage({
-				'username': 'Wumpus',
-				'attachments': [{
-					'pretext': 'this looks pretty cool',
-					'color': '#F0F',
-					'footer_icon': 'http://snek.s3.amazonaws.com/topSnek.png',
-					'footer': 'Powered by sneks',
-					'ts': timestamp
-				}]
-			}).catch(console.error);*/
-
-
-			/*
-			WEBHOOK.sendSlackMessage({
-				//'username': 'Wumpus',
-				'text': avatarURL,
-				'attachments': [{
-					//'pretext': '',
-					'color': '#FF8800',
-					//'footer_icon': '',
-					//'footer': '',
-					'ts': timestamp
-				}]
-			}).catch(console.error);*/
-		}
+	/*
+	WEBHOOK.sendSlackMessage({
+		//'username': 'Wumpus',
+		'text': avatarURL,
+		'attachments': [{
+			//'pretext': '',
+			'color': '#FF8800',
+			//'footer_icon': '',
+			//'footer': '',
+			'ts': timestamp
+		}]
+	}).catch(console.error);*/
+	
+	// if the message contains the BOT's mention...
+	if(message.content.includes(BOT.user.toString())) {
+		// .reply adds the user's mention to the begining of the message.
+		message.reply("Hello!");
 	}
 });
 
